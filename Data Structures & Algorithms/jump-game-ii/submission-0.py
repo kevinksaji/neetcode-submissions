07994f1a24a3f2e513d9e76ebfe4a88a74b52ef7
Kevin K Saji <142881962+kevinksaji@users.noreply.github.com>
2026-05-06 20:07:21 +0800

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        res = 0
+
+        l = r = 0
+
+        while r < len(nums) -1:
+            currJump = 0
+            for i in range(l, r + 1):
+                currJump = max(currJump, i + nums[i])
+            l = r + 1
+            r = currJump
+            res += 1
+
+
+        return res
+
+            
